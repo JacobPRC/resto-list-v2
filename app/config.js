660a6c02b5327873012c2e0856391f2c72b71843
jacob <jacobcunningham96@yahoo.com>
2022-02-16 15:20:41 -0800

    issue fixed

--- app/config.js
+++
@@ -1,8 +0,0 @@
-// Set up data base and all process.env stuff
-
-let config = module.exports;
-
-config.port = 5000;
-
-config.mongo =
-  "mongodb+srv://JacobPRC:12abczed88695todegy@cluster0.nurkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
